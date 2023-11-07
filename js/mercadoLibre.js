1885e916bd61e630825a0e0256e4c535443c5cab
Camilo3008 <camilo.alvarez3008@gmail.com>
2023-11-07 16:38:57 -0500

    añadiendo productos al mercado libre

--- js/mercadoLibre.js
+++ js/mercadoLibre.js
@@ -1,52 +1,95 @@
-let mainContent = document.getElementById("mainContent")
+let boxProductos = document.getElementById("boxProductos")
 
-let boxProducto = document.createElement("div")   //ingresamos un elemento, puede ser vualquier cosa.
-
-
-
-
-mainContent.appendChild(boxProducto)
-
-boxProducto.setAttribute("class", "box-producto")    //atributo y su respecctivo valor. 
+/* Contenedor productos individuales */
+let boxProducto = document.createElement("div")   //creamos un elemento, puede ser cualquier cosa.
 
 
 
+
+boxProductos.appendChild(boxProducto)         //que tiene de hijo a 
+boxProducto.setAttribute("class", "box-producto")    //atributo y su respecctivo valor,    
+
+
+let boxImagen = document.createElement("div")
+boxProducto.appendChild(boxImagen)
+boxImagen.setAttribute("class", "box-imagen")
+
+
+let imagenProducto = document.createElement("img")
+boxImagen.appendChild(imagenProducto)
+imagenProducto.setAttribute("src", "img/mazda1.webp")
+imagenProducto.setAttribute("class", "img-producto")
+
+
+/* informacion */
+
+let infoProducto = document.createElement("div")
+boxProducto.appendChild(infoProducto)
+infoProducto.setAttribute("class", "box-info-producto")
+
+
+
+/* marca */
 let marcaProducto = document.createElement("label")
-mainContent.appendChild(marcaProducto)
+infoProducto.appendChild(marcaProducto)
+marcaProducto.setAttribute("class", "info-principal")
 
-let nodoMarcaProducto = document.createTextNode("Mazda")
-
+let nodoMarcaProducto = document.createTextNode("Mazda ")
 marcaProducto.appendChild(nodoMarcaProducto)
 
-/* cargar modelo */
-/* cx-s */
+/* modelo */
+
+let modeloProducto = document.createElement("label")
+infoProducto.appendChild(modeloProducto)
+modeloProducto.setAttribute("class", "info-principal")
+
+let nodoModeloProducto = document.createTextNode(" Grand Touring Lx")
+modeloProducto.appendChild(nodoModeloProducto)
+
 /* precio */
+
+let precioProducto = document.createElement("label")
+infoProducto.appendChild(precioProducto)
+precioProducto.setAttribute("class", "precio")
+
+let nodoPrecioProducto = document.createTextNode("$ 65.000.000")
+precioProducto.appendChild(nodoPrecioProducto)
+
+
 /* año */
+
+let añoProducto = document.createElement("label")
+infoProducto.appendChild(añoProducto)
+añoProducto.setAttribute("class", "info-secundaria")
+
+let nodoAñoProducto = document.createTextNode("2017 °")
+añoProducto.appendChild(nodoAñoProducto)
+
+
 /* kilometraje */
+let kilometrajeProducto = document.createElement("label")
+infoProducto.appendChild(kilometrajeProducto)
+kilometrajeProducto.setAttribute("class", "info-secundaria")
+
+let nodoKilometrajeProducto = document.createTextNode(" 36.000 km °")
+kilometrajeProducto.appendChild(nodoKilometrajeProducto)
 
 
+/* ubicacion */
 
+let ubicacionProducto = document.createElement("label")
+infoProducto.appendChild(ubicacionProducto)
+ubicacionProducto.setAttribute("class", "info-secundaria")
 
-
-
-/* Cargar imagen */
-let imgProducto = document.createElement("img")
-
-mainContent.appendChild(imgProducto)
-imgProducto.setAttribute("src", "img/thumb1.png")
-
-
-
+let nodoUbicacionProducto = document.createTextNode(" Usaquen - Bogota D.C")
+ubicacionProducto.appendChild(nodoUbicacionProducto)
 
 
 
 /* 
-let marcaProducto = document.createElement("label")
-boxProducto.appendChild(marcaProducto)
 
-let nodoMarcaProducto = document.createTextNode("Mazda")
-nodoMarcaProducto.setAttribute("class", "parrafo-js")
+let boxImagen = document.createElement("img")
 
-
-marcaProducto.appendChild(nodoMarcaProducto)
- */
+boxProducto.appendChild(boxImagen)
+boxImagen.setAttribute("src", "img/mazda1.webp")
+boxImagen.setAttribute("class", "box-imagen") */
