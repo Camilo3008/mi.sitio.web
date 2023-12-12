5cad110a1b1925040e0a360b6b3d0ae65be9e82a
Camilo3008 <camilo.alvarez3008@gmail.com>
2023-12-12 16:40:23 -0500

    amazon

--- js/amazon.js
+++ js/amazon.js
@@ -8,6 +8,13 @@ let btnGraficar = document.getElementById("btnGraficar");
 
 
 
+let porcentajeCalificacion1 = document.getElementById("porcentajeCalificacion1")
+
+
+let boxTextoCalificacion1 = document.getElementById("boxTextoCalificacion1")
+
+
+
 
 btnGraficar.addEventListener("click", function(){
 
@@ -28,13 +35,27 @@ btnGraficar.addEventListener("click", function(){
     //porcentajes. 
 
     
-    let porcentaje1 = (calif1 * 100)/suma
-    let porcentaje2 = (calif2 * 100)/suma
-    let porcentaje3 = (calif3 * 100)/suma
-    let porcentaje4 = (calif4 * 100)/suma
-    let porcentaje5 = (calif5 * 100)/suma
+    let porcentaje1 = ((calif1 * 100)/suma).toFixed(0)
+    let porcentaje2 = ((calif2 * 100)/suma).toFixed(0)
+    let porcentaje3 = ((calif3 * 100)/suma).toFixed(0)
+    let porcentaje4 = ((calif4 * 100)/suma).toFixed(0)
+    let porcentaje5 = ((calif5 * 100)/suma).toFixed(0)
 
-    alert("los procetajes son: "+porcentaje1+"||"+porcentaje2+"||"+porcentaje3+"||"+porcentaje4+"||"+porcentaje5)
+   //alert("los procetajes son: "+porcentaje1+"||"+porcentaje2+"||"+porcentaje3+"||"+porcentaje4+"||"+porcentaje5)
+
+
+   porcentajeCalificacion1.style.width = (porcentaje1 * 2) + "px";
+   
+   
+   let spanPorcentaje = document.createElement("span")
+
+   boxTextoCalificacion1.appendChild(spanPorcentaje)
+
+   spanPorcentaje.innerHTML =promCalificacion1.toFixed(0) + "%"
+
+   spanPorcentaje.setAttribute("class", "span-porcentaje")
+
+
 
 
 })
---
+++ js/estrella.js
@@ -0,0 +1,6 @@
+let inputBusqueda = document.getElementById("inputBusqueda"); 
+let btnGraficar = document.getElementById("btnGraficar");
+
+let boxEstrella = document.getElementById("boxEstrellas")
+
+
